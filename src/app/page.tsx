1d4276e431493f2c9e037ace60d1da0b8bd52ec8
Tobias Albrecht <tobias.albrecht@albrecht.services>
2023-06-15 14:16:00 +0200

    Make example loading WC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
-import styles from './page.module.css'
-import dynamic from "next/dynamic";
+'use client'
 
-const CorbadoWebComponent = dynamic(
-    () => import("@corbado/webcomponent") as Promise<{ default: React.ComponentType<any> }>,
-    {ssr: false}
-);
+import styles from './page.module.css'
+import dynamic from 'next/dynamic'
+ 
+const CorbadoWebComponent = dynamic(() => import("@corbado/webcomponent"), {
+  loading: () => <p>Loading...</p>,
+})
+ 
 
 export default function Home() {
+
     return (
         <main className={styles.main}>
             <div className={styles.center}>
                 <h1>Corbado Example Next.js</h1>
-                <CorbadoWebComponent project_id={process.env.NEXT_PUBLIC_PROJECT_ID} conditional="yes">
-                    <input name="username" id="corbado-username" value="" required autoComplete="webauthn" autoFocus/>
-                </CorbadoWebComponent>
+                <corbado-auth project_id={process.env.NEXT_PUBLIC_PROJECT_ID} conditional="yes">
+                    <input name="username" id="corbado-username" required autoComplete="webauthn" autoFocus/>
+                </corbado-auth>
             </div>
         </main>
     )

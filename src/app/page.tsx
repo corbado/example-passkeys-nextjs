a5739c1e0827cb3d2df2013f5a86e1cecdfc5ca4
Vincent Delitz <vincent.delitz@corbado.com>
2023-06-16 23:27:41 +0200

    created first draft of working version with token validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,17 @@
 'use client'
 
-import styles from './page.module.css'
-import dynamic from 'next/dynamic'
-
-const CorbadoWebComponent = dynamic(() => import("@corbado/webcomponent"), {
-  loading: () => <p>Loading...</p>,
-})
- 
+import Head from "next/head";
 
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <div className={styles.center}>
-                <corbado-auth project_id={process.env.NEXT_PUBLIC_PROJECT_ID} conditional="yes">
-                    <input name="username" id="corbado-username" required autoComplete="webauthn" autoFocus/>
-                </corbado-auth>
-            </div>
-        </main>
+        <div>
+            <Head>
+                <script src="https://auth.corbado.com/auth.js" defer></script>
+            </Head>
+
+            <corbado-auth project_id={process.env.NEXT_PUBLIC_PROJECT_ID} conditional="yes">
+                <input name="username" id="corbado-username" required autoComplete="webauthn" />
+            </corbado-auth>
+        </div>
     )
 }

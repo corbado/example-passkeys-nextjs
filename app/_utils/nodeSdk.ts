bdc8c80d6e4a21bed0d435053298035fd7be0bc5
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2024-10-16 14:17:53 +0200

    Adapted code to new session management

--- app/_utils/nodeSdk.ts
+++ app/_utils/nodeSdk.ts
@@ -2,8 +2,10 @@ import { Config, SDK } from '@corbado/node-sdk';
 
 const projectID = process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID!;
 const apiSecret = process.env.CORBADO_API_SECRET!
+const frontendAPI = process.env.CORBADO_FRONTEND_API!;
+const backendAPI = process.env.CORBADO_BACKEND_API!;
 
-const config = new Config(projectID, apiSecret);
+const config = new Config(projectID, apiSecret, frontendAPI, backendAPI);
 const sdk = new SDK(config);
 
 export default function getNodeSDK() {
